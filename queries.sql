c04049aee8a82eeab71729655c96133182901366
Subrina7 <Subrinaf.7@gmail.com>
2023-04-14 15:09:46 +0100

    MVP and first extension done

--- queries.sql
+++ queries.sql
@@ -1,5 +1,15 @@
-SELECT name FROM animals WHERE enclosure_id = 2;
+-- SELECT name FROM animals WHERE enclosure_id = 2;
+
+-- SELECT * FROM employees
+-- INNER JOIN assignments
+-- ON employees.id = assignments.employee_id
+-- WHERE employee_id = 3;
+
 
 SELECT * FROM employees
 INNER JOIN assignments
-ON employees.id = assignments.employee_id;
+ON employees.id = assignments.employee_id
+INNER JOIN enclosures 
+ON assignments.enclosure_id = enclosures.id
+WHERE enclosures.closed_for_maintenance = 'TRUE';
+
